bb4a96879763321484a156d2514e28659dae8369
FP <franciszek.parma@gmail.com>
2026-05-31 14:26:45 +0200

    CHanged the forward pass input and the name of the class

--- model.py
+++ model.py
@@ -10,15 +10,15 @@ device = ('cuda' if torch.cuda.is_available()
           else 'cpu'
 )
 
-class Encoder(nn.Module):
+class RAGEtin(nn.Module):
   def __init__(self, name="jhu-clsp/ettin-encoder-150m"):
     super().__init__()
     
     self.enc = AutoModel.from_pretrained(name)
     self.enc.final_layer_norm = nn.Identity()
     
-  def forward(self, X):
-    return self.enc(**X).last_hidden_state[..., 0, :]
+  def forward(self, queries, positives, negatives):
+    return self.enc(**queries).last_hidden_state[..., 0, :], self.enc(**positives).last_hidden_state[..., 0, :], self.enc(**negatives).last_hidden_state[..., 0, :]
 
 token_adder = AddTokens()
 
@@ -26,5 +26,5 @@ token_adder = AddTokens()
 tokenizer = AutoTokenizer.from_pretrained("jhu-clsp/ettin-encoder-150m")
 tokenizer.add_tokens(list(token_adder.new_tokens.values()))
 
-model = Encoder().to(device)
+model = RAGEtin().to(device)
 model.enc.resize_token_embeddings(len(tokenizer))
